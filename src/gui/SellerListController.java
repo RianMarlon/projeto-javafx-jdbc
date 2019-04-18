f7e9018f494626b0eb29f1b07867e2113a60418a
Rian Marlon <rianmarlon20@yahoo.com>
2019-04-18 20:42:03 -0300

    SellerForm (dialog) designer

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,19 +1,28 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.entities.Seller;
@@ -50,8 +59,9 @@ public class SellerListController implements Initializable {
 	private ObservableList <Seller> obsList;
 	
 	@FXML
-	public void onBtNovoAction () {
-		System.out.println("onBtNovoAction");
+	public void onBtNovoAction (ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm ("/gui/SellerForm.fxml", parentStage);
 	}
 	
 	public void setSellerService (SellerService service) {
@@ -82,5 +92,23 @@ public class SellerListController implements Initializable {
 		List <Seller> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewVendedor.setItems(obsList);
+	}
+	
+	private void createDialogForm (String absoluteNome, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteNome));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage ();
+			dialogStage.setTitle("Digite os dados do Vendedor");
+			dialogStage.setScene(new Scene (pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro carregando a página", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
